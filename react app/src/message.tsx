28ab148055b26785853d1ec8cb8ad00b7808f62c
Osita <ositaodunze2@gmail.com>
2024-04-24 22:11:28 -0400

    initial commit

---
+++ react app/src/message.tsx
@@ -0,0 +1,10 @@
+// PacalCasing
+function Message() {
+    //JSX syntax: javascript XML
+    const name='mosh';
+    if (name)
+        return <h1>Hello world {name}</h1>;
+    return <h1>Hello world</h1>
+}
+
+export default Message
